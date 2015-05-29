2e67309eb7e1bb9efe135cdbda296fe59463556d
Brandon <BrandonZippe@users.noreply.github.com>
2015-05-29 16:30:49 -0500

    Create meta.php
    Head/Meta data

---
+++ meta.php
@@ -0,0 +1,16 @@
+<?php echo '
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/html1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-us">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="description" content="Brandon Zippe">
+    <meta name="keywords" content="Brandon Zippe">
+    <meta name="viewport" content="initial-scale=1.0, width=device-width, user-scalable=no">
+    <meta name="twitter:site" content="@brandonzippe">
+    <meta name="author" content="Brandon Zippe">
+    <link href="http://fonts.googleapis.com/css?family=Amatic+SC:400,700|Advent+Pro" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <title>Zippetomylou Designs</title>
+</head>
+';?>
