80c75ffe8373f6273d372a174512a51547a2b00e
Brandon <BrandonZippe@users.noreply.github.com>
2015-05-29 16:31:22 -0500

    Create footer.php
    footer/script data

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php echo '
+    <!-Footer->
+    <footer class="siteFooter">
+        <div class="footer">
+            <div class="copyright">&copy; www.zippetomylou.com - All Rights Reserved.</div>
+        </div>
+    </footer>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/generic.js"></script>
+' ;?>
