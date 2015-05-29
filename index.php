4f8c4f6a2c8956becf06c9921cc44d21f566390d
Brandon <BrandonZippe@users.noreply.github.com>
2015-05-29 16:29:36 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php include ( 'meta.php');?>
+
+<body class="site">
+    <!-hero header->
+    <header class="hero">
+        <?php include ( 'nav.php');?>
+        <h2 class="tag">What can we design for you?</h2>
+        <div id="greenBtn" class="heroBtn">Start Building</div>
+    </header>
+    <!-main content->
+    <main class="siteContent">
+        <!-flexbox mod->
+        <section class="flexBoxWrap">
+            <article class="flexBox">
+                <div class="flexBoxContent">
+                    <h1 class="fbTitle">Design.</h1>
+                    <div class="fbInnerTxt">
+                        <h2 class="subTitle">Our Designs...</h2>
+                        <p>aim to showcase your brand in the most accurate way possible. We listen, ask questions, and collaborate to bring you the best web solutions.</p>
+                        <div id="purpleBtn" class="contentBtn">Work We Designed</div>
+                    </div>
+                </div>
+            </article>
+            <article class="flexBox">
+                <div class="flexBoxContent">
+                    <h1 class="fbTitle">Create.</h1>
+                    <div class="fbInnerTxt">
+                        <h2 class="subTitle">Our Creations...</h2>
+                        <p>are born out of pure discovery. Once we have a grasp on what your brand and web expectations are, the sky is the limit as to what we can create.</p>
+                        <div id="greenBtn" class="contentBtn">Work We Created</div>
+                    </div>
+                </div>
+            </article>
+            <article class="flexBox">
+                <div class="flexBoxContent">
+                    <h1 class="fbTitle">Enjoy.</h1>
+                    <div class="fbInnerTxt">
+                        <h2 class="subTitle">We Enjoy...</h2>
+                        <p>refreshing your brands look and are committed to remaining by your side so you can enjoy the prosperities of an evolving web presence.</p>
+                        <div id="grayBtn" class="contentBtn">Work We Enjoyed</div>
+                    </div>
+                </div>
+            </article>
+            <div class="fbClose"><span class="icon-plus"></span></div>
+        </section>
+        <section class="about">
+            <article class="content">
+                <h2 class="contentTitle">Let's Make Something</h2>
+                <p>Our creative focus is fixated on getting it right. We listen, ask questions, and collaborate to make sure your brand is accurately communicated through our designs. Once we launch your project, you can count on us to stick around in order to keep evolving and maximizing your web presence.</p>
+                <p>While our specialty is your company’s website & online strategy, we’re not one’s to shy away from a challenge. From logos to socials, if you’re willing to bring it to the table we’re willing to make it happen. Simply put, we Design.Create.Enjoy.</p>
+                <div id="purpleBtn" class="contentBtn">Get Started</div>
+            </article>
+        </section>
+        <?php include ( 'socialsphere.php');?>
+    </main>
+    <?php include ( 'footer.php');?>
+</body>
+
+</html>
